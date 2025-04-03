8940b775e441cc021cc0b13aeeaec41529d6569a
Axel <axel.pribadi1403@gmail.com>
2025-04-03 08:50:21 +0800

    add gemini agent

--- chatbot/app/bot/app.py
+++ chatbot/app/bot/app.py
@@ -2,13 +2,14 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')))
 
-
 import chainlit as cl
 from agno.agent import Agent, AgentKnowledge, RunResponse
 from agno.models.openai import OpenAIChat
 from agno.vectordb.lancedb import LanceDb
 from agno.vectordb.search import SearchType
 from agno.embedder.openai import OpenAIEmbedder
+
+from agno.embedder.google import GeminiEmbedder
 
 from app.core.config import settings
 
@@ -21,7 +22,6 @@ knowledge_base = AgentKnowledge(
         embedder=OpenAIEmbedder(api_key=settings.OPENAI_API_KEY)
     )
 )
-
 agent = Agent(
     model=OpenAIChat(id="gpt-4o-mini", api_key=settings.OPENAI_API_KEY),
     knowledge=knowledge_base,
@@ -29,6 +29,17 @@ agent = Agent(
     search_knowledge=True,
     markdown=True
 )
+
+# GEMINI Agent
+knowledge_base = AgentKnowledge(
+    vector_db=LanceDb(
+        table_name=settings.LANCE_DB_TABLE_NAME,
+        uri=settings.LANCE_DB_URI,
+        search_type=SearchType.vector,
+        embedder=GeminiEmbedder(api_key=settings.GEMINI_API_KEY)
+    )
+)
+
 
 @cl.on_message
 async def on_message(message: cl.Message):
--- chatbot/app/core/config.py
+++ chatbot/app/core/config.py
@@ -2,20 +2,21 @@ from pydantic_settings import BaseSettings
 
 class Config(BaseSettings):
     # folder paths
-    DOWNLOADS_FOLDER_PATH: str
-    PROCESSED_FOLDER_PATH: str
-    FAILED_FOLDER_PATH: str
+    # DOWNLOADS_FOLDER_PATH: str
+    # PROCESSED_FOLDER_PATH: str
+    # FAILED_FOLDER_PATH: str
 
     # LanceDB
     LANCE_DB_URI: str
     LANCE_DB_TABLE_NAME: str
 
     # Chunking Variables
-    CHUNK_SIZE: int
-    CHUNK_OVERLAP: int
+    # CHUNK_SIZE: int
+    # CHUNK_OVERLAP: int
 
     # OpenAI
     OPENAI_API_KEY: str
+    GEMINI_API_KEY: str
 
     model_config = {
         "env_file":".env", 
